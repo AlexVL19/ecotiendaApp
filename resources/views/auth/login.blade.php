5a991fe1d539609d3ea32d1de45f01a03ff6201d
AlexVL19 <avalencia190@misena.edu.co>
2022-03-24 23:15:29 -0500

    Diseño de login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,6 +4,23 @@
 
 @section('contents')
 
-<h1>Formulario de ingreso</h1>
+<div class="container border rounded mt-5" style="width: 450px">
+    <h3 class="text-center mt-2">Ingreso al sistema</h3>
+    <br>
+    <form method="POST" acti>
+        <div class="form-group">
+          <label for="exampleInputEmail1">Correo electrónico</label>
+          <input type="email" class="form-control" id="email" name="email" placeholder="Escriba su correo...">
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Contraseña</label>
+          <input type="password" class="form-control" id="pass" name="pass" placeholder="Escriba su contraseña...">
+        </div>
+        <br>
+        <button type="submit" class="btn btn-success">Ingresar</button>
+        <br>
+        <br>
+      </form>
+</div>
 
 @endsection
